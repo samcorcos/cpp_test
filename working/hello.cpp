4078d290d1b9d9d6a0e3641b4a8b9af27ca54830
Sam Corcos <samcorcos@gmail.com>
2015-01-07 16:31:46 -0500

    hello world

---
+++ working/hello.cpp
@@ -0,0 +1,16 @@
+//
+//  hello.cpp
+//  working
+//
+//  Created by xxxx on 1/7/15.
+//  Copyright (c) 2015 xxxx. All rights reserved.
+//
+
+#include <cstdio>
+using namespace std;
+
+int main( int argc, char ** argv )
+{
+    puts("Hello, World!");
+    return 0;
+}
---
+++ working/hello.h
@@ -0,0 +1,14 @@
+//
+//  hello.h
+//  working
+//
+//  Created by xxxx on 1/7/15.
+//  Copyright (c) 2015 xxxx. All rights reserved.
+//
+
+#ifndef __working__hello__
+#define __working__hello__
+
+#include <stdio.h>
+
+#endif /* defined(__working__hello__) */
