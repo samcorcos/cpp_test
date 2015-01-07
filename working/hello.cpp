6ec07feb3166ef05cc41c597f2b21a2043c8865e
Sam Corcos <samcorcos@gmail.com>
2015-01-07 17:34:34 -0500

    string arrays

--- working/hello.cpp
+++ working/hello.cpp
@@ -3,35 +3,50 @@ using namespace std;
 
 int main( int argc, char ** argv )
 {
-    int x = 7;
-    int *ip = &x;
-    int &y = x;
-    printf("the value of x is %d\n", x);
-    printf("the value of y is %d\n", y);
-    printf("the value of *ip is %d\n", *ip);
-
-    x = 42;
-    printf("the value of x is %d\n", x);
-    printf("the value of y is %d\n", y);
-    printf("the value of *ip is %d\n", *ip);
-    
-    y = 73;
-    printf("the value of x is %d\n", x);
-    printf("the value of y is %d\n", y);
-    printf("the value of *ip is %d\n", *ip);
-    
+    char s[] = {'s','t','r','i','n','g',0};
+    // You would get the same result if you defined it as follows:
+    // `char s[] = "string";` Which is treated just like the array above.
+    for (int i = 0; s[i]; ++i) {
+        printf("char is %c\n", s[i]);
+    }
     return 0;
 }
 
 
 /*
- `int &y = x;` makes it `y` a reference, and the *ip = &x makes it a pointer...?
- One of the major differences is that you cannot define a reference without initializing it.
- So, in this case, whenever you use `y`, it will always have the same value as `x`, and it will give you the value of x at the time when it's called
+Arrays
  
- When you change hte valye of x, it changes the value of y
- and when you change y, it changes x. When somethign is a reference, it's pointing to the same piece of memory
+ An array is a fixed-size container
+ `int ia[5];` is an int index of length 5
  
+ Array elements can also be accessed as if the array were a pointer. The two statements are equivalent:
+ `ia[0] = 1;`
+    and
+ `*ia = 1;`
+ 
+ Arrays can be accessed as if they are pointers
+ 
+ 
+ `int *ip = ia;` makes `ip` a reference to `ia`
+ so, `*ip = 2;` is a reference to the sec
+ 
+ In C++, pointers are strongly typed, so they know the size of what they point to.
+ 
+ So when you increment a pointer, it always increments by the size of the object it's defined with.
+ So this assigns the value 3 to the second element of the array. 
+ 
+ `++ip;`
+ 
+ `*ip = 3;` This assigns 3 to the second value of the array, if you previously incremented it
+ 
+ `*(++ip) = 4;`
+ This is a common way to increment and define it at the same time.
+ 
+ You can also define them using an initializer list:
+ 
+ `int ia[5] = {1,2,3,4,5};`
+ 
+ In C++ a C-String is simply an array of characters terminated with a 0. This is sometimes called a null-terminated string
  
  
  
